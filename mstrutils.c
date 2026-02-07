f41a7426b1e32ea4ca32a383b4f904aa75018deb
tafabey <muhammedmustafademir@proton.me>
2026-02-07 14:32:27 +0000

    Added mstrcmp function

--- mstrutils.c
+++ mstrutils.c
@@ -7,3 +7,14 @@ int mstrlen(const char *str) {
 
     return len;
 }
+
+int mstrcmp(const char *str1, const char *str2) {
+    for (int i = 0; ; i++) {
+        if (str1[i] != str2[i])
+            return 1;
+        if (str1[i] == '\0')
+            return 0;
+    }
+
+    return 0;
+}
--- mstrutils.h
+++ mstrutils.h
@@ -2,5 +2,6 @@
 #define MSTRUTILS_H
 
 int mstrlen(const char *str);
+int mstrcmp(const char *str1, const char *str2);
 
 #endif
