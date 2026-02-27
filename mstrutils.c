530a3043fae64fa3ae526ff3558f531a31c0b902
tafabey <muhammedmustafademir@proton.me>
2026-02-27 20:48:25 +0000

    Fixed indentation problem

--- mstrutils.c
+++ mstrutils.c
@@ -9,11 +9,11 @@ int mstrlen(const char *str) {
 }
 
 int mstrcmp(const char *str1, const char *str2) {
-    for (int i = 0; ; i++) {
+    for (int i = 0;; i++) {
         if (str1[i] != str2[i])
-	    return str1[i] - str2[i];
-	
-	if (str1[i] == '\0')
+            return str1[i] - str2[i];
+
+        if (str1[i] == '\0')
             return 0;
     }
 
@@ -31,26 +31,26 @@ char *mstrcpy(char *dst, const char *src) {
 
 char *mstrchr(const char *str, int ch) {
     while (str++) {
-	if (*str == ch)
-	    return (char*) str;
+        if (*str == ch)
+            return (char*)str;
     }
-    
+
     return 0;
 }
 
 char *mstrstr(const char *str, const char *substr) {
     int ctr = 0;
-    
+
     while (str++) {
-	if (*str == substr[ctr]) {
-	    ctr++;
-	} else {
-	    ctr = 0;
-	}
-	
-	if (ctr == mstrlen(substr)) {
-	    return (char*) str - ctr + 1;
-	}
+        if (*str == substr[ctr]) {
+            ctr++;
+        } else {
+            ctr = 0;
+        }
+
+        if (ctr == mstrlen(substr)) {
+            return (char*)str - ctr + 1;
+        }
     }
 
     return "";
@@ -58,10 +58,10 @@ char *mstrstr(const char *str, const char *substr) {
 
 int mstrncmp(const char *str1, const char *str2, int n) {
     for (int i = 0; i < n; i++) {
-	if (str1[i] != str2[i])
-	    return str1[i] - str2[i];
-	if (str1[i] == '\0' || str2[i] == '\0')
-	    break;
+        if (str1[i] != str2[i])
+            return str1[i] - str2[i];
+        if (str1[i] == '\0' || str2[i] == '\0')
+            break;
     }
 
     return 0;
@@ -70,62 +70,64 @@ int mstrncmp(const char *str1, const char *str2, int n) {
 char *mstrncpy(char *dst, const char *src, int n) {
     int i;
     for (i = 0; i < n; i++)
-	dst[i] = src[i];
+        dst[i] = src[i];
 
-     dst[i] = '\0';
-     return dst;
+    dst[i] = '\0';
+    return dst;
 }
 
 char *mstrcat(char *dst, const char *src) {
     int dstlen = mstrlen(dst);
-    
-    for (int i = 0; src[i] != '\0'; i++)
-	dst[dstlen + i] = src[i];
-    
-    return (char *) dst;
+
+    for (int i = 0; src[i] != '\0'; i++) {
+        dst[dstlen + i] = src[i];
+    }
+
+    return (char*)dst;
 }
 
 char *mstrncat(char *dst, const char *src, int n) {
     int dstlen = mstrlen(dst);
-    
-    for (int i = 0; src[i] != '\0' && i < n; i++)
-	dst[dstlen + i] = src[i];
-    
-    return (char *) dst;
+
+    for (int i = 0; src[i] != '\0' && i < n; i++) {
+        dst[dstlen + i] = src[i];
+    }
+
+    return (char*)dst;
 }
 
 void *mmemcpy(void *dst, const void *src, int n) {
-    char *ptr1 = (char *) dst;
-    char *ptr2 = (char *) src;
+    char *ptr1 = (char*)dst;
+    char *ptr2 = (char*)src;
     for (int i = 0; i < n; i++) {
-	*ptr1++ = *ptr2++;
+        *ptr1++ = *ptr2++;
     }
 
     return dst;
 }
 
 int mmemcmp(const void *ptr1, const void *ptr2, int n) {
-    char *cp1 = (char *) ptr1;
-    char *cp2 = (char *) ptr2;
+    char *cp1 = (char*)ptr1;
+    char *cp2 = (char*)ptr2;
     for (int i = 0; i < n; i++)
-	if (cp1[i] != cp2[i])
-	    return cp1[i] - cp2[i];
+        if (cp1[i] != cp2[i])
+            return cp1[i] - cp2[i];
 
     return 0;
 }
 
 void *mmemmove(void *dst, const void *src, int n) {
-    char *ptr1 = (char *) dst;
-    char *ptr2 = (char *) src;
+    char *ptr1 = (char*)dst;
+    char *ptr2 = (char*)src;
     if (dst < src) {
-	for (int i = 0; i < n; i++) {
-	    ptr1[i] = ptr2[i];
-	}
+        for (int i = 0; i < n; i++) {
+            ptr1[i] = ptr2[i];
+        }
     } else {
-	for (int i = n - 1; i >= 0; i--) {
-	    ptr1[i] = ptr2[i];
-	}
+        for (int i = n - 1; i >= 0; i--) {
+            ptr1[i] = ptr2[i];
+        }
     }
-    
+
     return dst;
 }
